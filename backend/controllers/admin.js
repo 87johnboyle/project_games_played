4b89575f0af2c48d205646b27ba9e202db23fb7a
87johnboyle <john.hazukisan@gmail.com>
2020-10-07 23:14:36 +0100

    Back-End fully working

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,7 +1,3 @@
-exports.getIndex = (req, res) => {
-    res.status(200).render('index');
-};
-
 const Game = require('../models/Game');
 
 exports.getIndex = async (req, res) => {
@@ -29,7 +25,7 @@ exports.getGame = async (req, res) => {
 };
 
 exports.getAddGame = (req, res) => {
-    res.status(200).render('edit-game');
+    res.status(200).render('edit-game', {editing: false});
 };
 
 exports.postGame = (req, res) => {
@@ -39,4 +35,61 @@ exports.postGame = (req, res) => {
     game.save();
     console.log('Game Added to the database');
     res.status(201).redirect('/');
+};
+
+exports.postDelete = async (req, res) => {
+    const gameId = req.body.gameId;
+
+    const game = await Game.findByIdAndRemove(gameId, (data) => data);
+
+    try {
+        console.log(game);
+        console.log('Item Deleted');
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.postEditGame = (req, res) => {
+    const gameId = req.body.gameId;
+    const { name, image, description } = req.body;
+
+    Game.findById(gameId)
+        .then((game) => {
+            game.name = name;
+            game.image = image;
+            game.description = description;
+
+            return game.save();
+        })
+        .then(() => {
+            console.log('Item Updated');
+            res.status(201).redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+exports.getEditGame = async (req, res) => {
+    const gameId = req.params.gameId;
+
+    const editMode = req.query.edit;
+
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    const game = await Game.findById(gameId);
+
+    try {
+        if (!gameId) {
+            return res.redirect('/');
+        }
+        console.log(game);
+        res.status(200).render('edit-game', { game: game, editing: editMode });
+    } catch (error) {
+        console.log(error);
+    }
 };
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -7,8 +7,14 @@ router.get('/', adminController.getIndex);
 
 router.get('/add-game', adminController.getAddGame);
 
-router.post('/add-game', adminController.postGame);
+router.get('/edit-game/:gameId', adminController.getEditGame)
 
 router.get('/:gameId', adminController.getGame);
+
+router.post('/add-game', adminController.postGame);
+
+router.post('/edit-game', adminController.postEditGame)
+
+router.post('/delete', adminController.postDelete);
 
 module.exports = router
